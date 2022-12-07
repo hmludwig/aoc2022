e266fb79bb5ef838951dbf3c1a834b7b523d34b7
Harald Ludwig <harald.ludwig@mailbox.org>
2022-12-07 11:38:23 +0100

    Solved Day07

--- README.md
+++ README.md
@@ -11,7 +11,7 @@ My solutions for [Advent of Code 2022](https://adventofcode.com/2022/).
 |  [04](https://adventofcode.com/2022/day/4) | :star: | :star: | [:page_facing_up:](https://github.com/hmludwig/aoc2022/blob/main/src/day04.py)
 |  [05](https://adventofcode.com/2022/day/5) | :star: | :star: | [:page_facing_up:](https://github.com/hmludwig/aoc2022/blob/main/src/day05.py)
 |  [06](https://adventofcode.com/2022/day/6) | :star: | :star: | [:page_facing_up:](https://github.com/hmludwig/aoc2022/blob/main/src/day06.py)
-|  [07](https://adventofcode.com/2022/day/7) |  |  | [:page_facing_up:](https://github.com/hmludwig/aoc2022/blob/main/src/day07.py)
+|  [07](https://adventofcode.com/2022/day/7) | :star: | :star: | [:page_facing_up:](https://github.com/hmludwig/aoc2022/blob/main/src/day07.py)
 |  [08](https://adventofcode.com/2022/day/8) |  |  | [:page_facing_up:](https://github.com/hmludwig/aoc2022/blob/main/src/day08.py)
 |  [09](https://adventofcode.com/2022/day/9) |  |  | [:page_facing_up:](https://github.com/hmludwig/aoc2022/blob/main/src/day09.py)
 |  [10](https://adventofcode.com/2022/day/10) |  |  | [:page_facing_up:](https://github.com/hmludwig/aoc2022/blob/main/src/day10.py)
---
+++ src/day07.py
@@ -0,0 +1,57 @@
+import sys
+from collections import defaultdict
+
+part1 = 0
+part2 = 0
+
+f = open(sys.argv[1])
+data = [x.strip() for x in f.readlines()]
+
+path = ''
+file_system = defaultdict(list)
+for line in data:
+    line = line.split()
+    if line[0] == '$' and line[1] == 'cd':
+        if line[2] == '..':
+            path = path.split('/')
+            path = '/'.join(path[:-1])
+            if path == '':
+                path = '.'
+        elif line[2] == '/':
+            path = '.'
+        elif path == '/':
+            path += line[2]
+        else:
+            path += '/' + line[2]
+    elif line[0] == '$':
+        continue
+    elif line[0] == 'dir':
+        file_system[path] += defaultdict(list)
+    else:
+        file_system[path].append(int(line[0]))
+
+folders = defaultdict(lambda : 0)
+for key in file_system.keys():
+    folders[key] += sum(file_system[key])
+
+for key in reversed(sorted(file_system.keys(), key = lambda key: len(key))):
+    if key == '.':
+        continue
+    tmp = key.split('/')
+    tmp = '/'.join(tmp[:-1])
+    folders[tmp] += folders[key]
+   
+for value in folders.values():
+    if value <= 100000:
+        part1 += value
+
+space = 70000000
+free_space = space - folders['.']
+space_needed = 30000000
+for value in sorted(folders.values()):
+    if value >= space_needed - free_space:
+        part2 = value
+        break
+
+print(f'Part 1: {part1}')
+print(f'Part 2: {part2}')
